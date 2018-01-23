7c458ee09bc098f8ae977a7fcf27763e2cfc0498
zanagi <yuanqi.shan@gmail.com>
2018-01-23 13:15:28 +0200

    Init basic shooting mechanics

---
+++ Assets/_Scripts/Game/Avian.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Avian : MonoBehaviour {
+
+    public Rigidbody2D rBody { get; private set; }
+    private bool touched;
+
+	// Use this for initialization
+	void Start () {
+        rBody = GetComponent<Rigidbody2D>();
+        rBody.Sleep();
+	}
+}
---
+++ Assets/_Scripts/Game/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    Idle, Animation
+}
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField]
+    private float force = 5.0f;
+    private GameState state;
+    private Avian selectedAvian;
+    
+	// Update is called once per frame
+	void Update ()
+    {
+        CheckTouch();
+	}
+
+    private void CheckTouch()
+    {
+        if (state != GameState.Idle)
+            return;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            var hit = Physics2D.Raycast(
+                new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, 
+                Camera.main.ScreenToWorldPoint(Input.mousePosition).y), Vector2.zero, 0f);
+
+            if (hit.collider)
+            {
+                Debug.Log("Selected.");
+                selectedAvian = hit.collider.GetComponentInParent<Avian>();
+            }
+        } else if(Input.GetMouseButtonUp(0) && selectedAvian)
+        {
+            Debug.Log("Released.");
+            var flightForce = (Input.mousePosition - selectedAvian.transform.position) * force;
+            selectedAvian.rBody.AddForce(flightForce);
+        }
+    }
+}
