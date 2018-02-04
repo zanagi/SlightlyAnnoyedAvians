6c153733aeaa9453d4b4899a129b47ddcba71c8b
zanagi <zanagiys@gmail.com>
2018-02-04 15:59:03 +0200

    Import background art, create loop move script

---
+++ Assets/LoopMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A script that moves its gameobject along x-axis and teleports it back to its starting point once its passed the declared maximum
+/// </summary>
+public class LoopMove : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 3;
+    [SerializeField]
+    private float endX = 15;
+    [SerializeField]
+    private float startX = -15;
+    
+	void FixedUpdate ()
+    {
+        transform.localPosition += Vector3.right * speed * Time.fixedDeltaTime;
+
+        if(transform.localPosition.x >= endX)
+        {
+            transform.localPosition = new Vector3(startX, transform.localPosition.y);
+        }
+	}
+}
--- README.txt
+++ README.txt
@@ -1,1 +1,8 @@
 Yuanqi Shan, 427337
+
+Background Art:
+	- Created on Inkscape
+	- Ground: Brown rectangle with an organic image filter
+	- Grass: Custom green area with a film grain filter
+	- Clouds: Custom drawn white areas with blur filter
+		- In the game, clouds also have a script that moves them attached
